d14be9587b637ebb080c3086183cd0e3ec763105
Kieran McLean <kieran.mclean@uon.edu.au>
2019-04-25 07:42:19 +0000

    Initial attempt at rotation cipher. Yet to add function.

--- main.c
+++ main.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+
+void rotationCipher(char rotationCipher[], int n);
 
 int main() {
-
+    
+    char plainText[200];
+    int key;
+    
+    printf("Please enter message to be encrypted (in capital letters): ");
+    scanf("%c", plainText);
+    
+    printf("Please enter value for key: ");
+    scanf("%d", key);
+    
+    printf("The message to be encrypted is: %c", plainText);
+    
+    rotationCipher(plainText, key);
+    
+    printf("The encrypted message is: %d", plainText);
+    
 return 0;
 }
+
+void rotationCipher(char rotationCipher[], int n)
+
